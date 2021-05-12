0ff808d2d2574cb5d94f1e6700bb9fac688dcc4e
coltenappleby <colten.appleby@gmail.com>
2021-05-12 12:17:42 -0400

    created comment create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+    def create
+        comment = Comment.create(create_params)
+        if comment.valid?
+            render json: comment
+        else
+            render json: comment.errors.full_messages
+        end
+
+    end
+
+    private
+    def create_params
+        params.permit(:user_id, :post_id, :content)
+    end
+
 end
