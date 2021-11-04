e811b95e35dc048a34621c78d54d3cf2d50f438a
coltenappleby <colten.appleby@gmail.com>
2021-11-04 17:14:20 -0400

    added localhost, heroku frontend, and deleted everything

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3001/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  allow do
+    origins 'https://baseball-lowlights-fe.herokuapp.com/'
 
     resource '*',
       headers: :any,
